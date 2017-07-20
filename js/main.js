f0d26a2443689d834a81230e7d6b22302cbb5651
Bruno Carvalho <bruno@brunocarvalho.me>
2017-07-20 17:08:12 -0300

    alteracoes dia 20

--- js/main.js
+++ js/main.js
@@ -19,11 +19,29 @@ $(document).ready(function(){
 
 	$(".menu-left").click(function(){
 		$(".content-menu").toggleClass("animacao-menu");
+		$(".content-menu-right").toggleClass("animacao-menu-right");
 		$(".icon-menu").toggleClass("hide");
 		$(".icon-menu-close").toggleClass("visible");
+		$(".menu-list ul").toggleClass("animacao-menu-list");
 	});
 
-
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
 	
+	$('.menu-left').click(function(){
+		var hamburger = $('.hamburger');
+		hamburger.toggleClass('ativo');
+		header.toggleClass('open');
+		if (hamburger.hasClass('ativo')) {
+			hamburger.find('.texto').html('fechar');
+		} else {
+			hamburger.find('.texto').html('menu');
+		}
+	});
+
 
 });
