215b6a63554cd25060c2757f8d027c818f84837f
Bruno Carvalho <bruno@brunocarvalho.me>
2017-07-25 17:39:49 -0300

    update 4

--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
+var time = 100; // Tempo de digitação em mili segundos
+var atual = 0; // indice atual do bloco de texto
+var qtd; //Definimos uma variavel
+ 
+$(function(){       
+    $('.textosTroca .item').hide();        
+    qtd = $('.textosTroca .item').length;       
+    setTimeout('digitaTexto('+atual+')',500);   
+});   
+ 
+function digitaTexto(item){
+    $('.areaTexto').html('');
+    objItem = $('.textosTroca .item').eq(item);        
+    texto = ($.trim(objItem.html()));       
+    if(!objItem.attr('texto')){           
+        objItem.attr('texto',texto);            
+        objItem.html('');       
+    }               
+    stringItem = new String(objItem.attr('texto'));               
+    setTimeout('efeitoDigita(stringItem,0)',time);   
+} 
+ 
+function efeitoDigita(stringItem,n){       
+    qtdLetras = stringItem.length;        
+    $('.areaTexto').append(stringItem[n]);               
+    if((n+1) < qtdLetras){           
+        setTimeout('efeitoDigita(stringItem,'+(n+1)+')',time);       
+    }else{           
+        if((atual+1) < qtd){               
+            atual++           
+        }else{               
+            atual = 0;           
+        }           
+        setTimeout('digitaTexto('+atual+')',2000);       
+    }           
+}
+
 $(document).ready(function(){
 /*
-
-	
 	$('.liness').addClass('animacao-lines');
 
 	$('.page-name').addClass('animacao-titulopagina-boot');
